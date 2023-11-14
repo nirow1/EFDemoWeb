9ca9debecd52a9f442b741e49657c26c0c505b7e
will <gunmonyk@gmail.com>
2023-11-14 23:54:18 +0100

    Add project files.

---
+++ EFDataAccess/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccess.Models
+{
+    internal class Address
+    {
+    }
+}
